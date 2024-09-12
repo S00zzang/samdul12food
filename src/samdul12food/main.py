2fdce21bafb88d0c50de887324cf63bd6ea7244f
S00zzang <sujinpark8719561@gmail.com>
2024-09-12 16:43:21 +0900

    docker fastapi

---
+++ src/samdul12food/main.py
@@ -0,0 +1,15 @@
+from typing import Union
+from fastapi import FastAPI
+import pickle
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+	return{"hello": "n12"}
+
+@app.get("/food")
+def food(name: str):
+	#시간을 구함
+	#음식 이름과 시간을 csv 파일로 저장 -> /code/data/food.csv
+	return{"food": name, "time": "2024-09-15 11:12:13"}
