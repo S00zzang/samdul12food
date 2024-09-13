6daca8d4b6affd89097f70343d171fe727fc6ecf
S00zzang <sujinpark8719561@gmail.com>
2024-09-13 10:27:22 +0900

    add main.py

--- src/samdul12food/main.py
+++ src/samdul12food/main.py
@@ -12,4 +12,6 @@ def read_root():
 def food(name: str):
 	#시간을 구함
 	#음식 이름과 시간을 csv 파일로 저장 -> /code/data/food.csv
+	#DB 저장
+	print("=======================" + name)
 	return{"food": name, "time": "2024-09-15 11:12:13"}
