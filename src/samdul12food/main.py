8d47693a6d9ca2ed88cf8b8b39881e78a8d1079f
S00zzang <sujinpark8719561@gmail.com>
2024-09-13 15:05:52 +0900

    add time name

--- src/samdul12food/main.py
+++ src/samdul12food/main.py
@@ -2,7 +2,7 @@ from typing import Union
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from datetime import datetime
+from datetime import datetime, timedelta
 import csv
 import os
 
@@ -18,7 +18,7 @@ app.add_middleware(
 )
 
 #CSV 파일 경로
-CSV_FILE_PATH = '/code/samdul12food/food.csv'
+CSV_FILE_PATH = '~/code/samdul12food/data/food.csv'
 
 @app.get("/")
 def read_root():
@@ -26,16 +26,21 @@ def read_root():
 
 @app.get("/food")
 def food(name: str):
+	#kst = now + 9h
+	kst_time = datetime.now() + timedelta(hours=9)
 	#시간을 구함
-	current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+	current_time = kst_time.strftime('%Y-%m-%d %H:%M:%S')
 
-	#음식 이름과 시간을 csv 파일로 저장 -> /code/samdul12food/food.csv
+	#파일 없으면 생성
+	os.makedirs(os.path.dirname(CSV_FILE_PATH), exist_ok=True)
+
+	#음식 이름과 시간을 csv 파일로 저장 -> /code/samdul12food/data/food.csv
 	file_exists = os.path.isfile(CSV_FILE_PATH)
 
 	with open(CSV_FILE_PATH, mode='a', newline='', encoding='utf-8') as file:
 		writer = csv.writer(file)
 		if not file_exists:
-			writer.writerow(['Timestamp'], ['Food Name'])
+			writer.writerow(['Timestamp', 'Food Name'])
 		writer.writerow([current_time, name])
 	
 	#DB 저장
